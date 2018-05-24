aa8c2828132ca96fd117f5fd711b4996cee21977
gz65555 <gz65555@126.com>
2018-05-24 22:59:12 +0800

    delete

--- src/controller/user.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const service_1 = require("../service");
-async function login(ctx, next) {
-    ctx.body = await service_1.service.user.login(ctx.request.body.unionId);
-}
-exports.login = login;
-//# sourceMappingURL=user.controller.js.map
